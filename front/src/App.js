dd4ad0783a629831179822ef8967328890e64441
Gayeon Lee <165263888+on22y@users.noreply.github.com>
2025-11-22 19:27:39 +0900

    feat: interview video 화면 생성 (#12)

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Main-Auth-Navbar/Login';
 import SignUp from './components/Main-Auth-Navbar/SignUp';
 import Home from './components/Home_PopUp/Home';
+import InterviewRecord from './components/Interview/InterviewRecord';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/interview/:id/webcam" element={<InterviewRecord />} />
       </Routes>
     </BrowserRouter>
   );
