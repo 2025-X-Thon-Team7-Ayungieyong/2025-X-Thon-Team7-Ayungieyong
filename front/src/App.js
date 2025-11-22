11fc71e8650140fffc96252a5f56d0a82b5e9b28
Jang Eunjae <167750756+eae22@users.noreply.github.com>
2025-11-22 17:23:57 +0900

    feat: Home.js, PopUp.js 기본 틀 구현 및 폴더 정리 (#9)

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Login from './components/Main-Auth-Navbar/Login';
+import SignUp from './components/Main-Auth-Navbar/SignUp';
+import Home from './components/Home_PopUp/Home';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
